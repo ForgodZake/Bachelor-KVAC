3bac425c7bb77f811e87228dc193601d1018f0cb
Zakke <forgodzake@gmail.com>
2026-03-16 13:34:38 +0100

    initial setup

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from charm.toolbox.pairinggroup import PairingGroup, G1, G2, ZR, pair
+
+group = PairingGroup('SS512')
+
+g1 = group.random(G1)
+g2 = group.random(G2)
+a = group.random(ZR)
+
+result = pair(g1 ** a, g2)
+
+print("Pairing works:", result)
